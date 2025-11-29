b033aecef6872486d6803fea98162e25f44dee1f
shariquejsr <shariquerafique30@gmail.com>
2025-11-29 19:40:03 +0530

    removed folder from github

--- Grat/src/main/java/GTPRac/Grat/Repository/RepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package GTPRac.Grat.Repository;
-
-import GTPRac.Grat.Entity.Employess;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryInterface extends JpaRepository<Employess,Integer> {
-}
